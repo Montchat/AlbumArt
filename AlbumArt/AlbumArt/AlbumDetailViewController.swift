6ae2c49e9d184ae7f3b09d6b75239bf1b7fc2e60
Montchat <je4440@gmail.com>
2015-10-14 16:53:52 -0400

    Added Play Button, Sound, and Link to iTunes
    - added play button
    - added pricing with link to iTunes
    - added play functionality

--- AlbumArt/AlbumArt/AlbumDetailViewController.swift
+++ AlbumArt/AlbumArt/AlbumDetailViewController.swift
@@ -7,9 +7,13 @@
 //
 
 import UIKit
+import AFNetworking
 
 class AlbumDetailViewController: UIViewController {
     var albumInfo: Album!
+    var tracks: [Track] = []
+    
+    let tracksData = TracksDataSource()
 
     @IBOutlet weak var albumImageView: UIImageView!
     @IBOutlet weak var albumNameLabel: UILabel!
@@ -17,11 +21,22 @@ class AlbumDetailViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tracksTableView.dataSource = tracksData
+    
         albumNameLabel.text = albumInfo.albumName
         albumImageView.image = albumInfo.albumImage ?? albumInfo.getImage()
         albumImageView.contentMode = .ScaleAspectFit
         
-
+        if let albumID = albumInfo.albumID {
+            print(albumID)
+            tracksData.FindTracksForAlbum("\(albumID)", completion: { () -> () in
+                self.tracksTableView.reloadData()
+                
+            })
+            
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -29,14 +44,96 @@ class AlbumDetailViewController: UIViewController {
         
 
     }
-    
-
 
     // MARK: - Navigation
-
 
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
 
     }
+    
+}
+
+class TracksDataSource: NSObject, UITableViewDataSource {
+    var tracks: [Track] = []
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("TrackCell", forIndexPath: indexPath) as! TrackCell
+        let track = tracks[indexPath.row]
+        
+        cell.trackInfo = track
+        cell.trackNameLabel.text = track.trackName
+        
+        return cell
+    
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return tracks.count
+    }
+    
+    let requestManager = AFHTTPRequestOperationManager()
+    
+    func FindTracksForAlbum(albumID: String, completion: () -> ()) {
+        tracks = []
+        
+        let urlString = "https://itunes.apple.com/lookup?id=\(albumID)&entity=song"
+        print(urlString)
+        
+        requestManager.GET(urlString, parameters: nil, success: { (operation, data) -> Void in
+            if let foundInfo = data as? [String:AnyObject] {
+                if let results = foundInfo["results"] as? [Dictionary] {
+                    for result in results {
+                        if result["trackName"] != nil {
+                            let track = Track(info: result)
+                            self.tracks.append(track)
+                        }
+                        
+                    }
+                    
+                    completion()
+                    
+                }
+                
+            }
+            
+            }) { (operation, error) -> Void in
+                print(error)
+                
+        }
+        
+    }
+    
+}
+
+class Track: NSObject {
+    var trackNumber: Int?
+    var trackName: String?
+    var trackPrice: Double?
+    var mediaURL: String?
+    var mediaData: NSData?
+    var trackViewURL: String?
+    
+    init(info:Dictionary) {
+        trackName = info["trackName"] as? String
+        trackNumber = info["trackNumber"] as? Int
+        trackPrice = info["trackPrice"] as? Double
+        mediaURL = info["previewUrl"] as? String
+        trackViewURL = info["trackViewUrl"] as? String 
+    
+    }
+    
+    func getMedia() -> NSData? {
+        if let mediaURLString = NSURL(string: mediaURL ?? "") {
+            mediaData = NSData(contentsOfURL: mediaURLString)
+            return mediaData
+            
+        }
+        
+        return nil
+        
+    }
+
 
 }
--- AlbumArt/AlbumArt/TrackCell.swift
+++ AlbumArt/AlbumArt/TrackCell.swift
@@ -7,12 +7,82 @@
 //
 
 import UIKit
+import SVGPlayButton
+import AVFoundation
 
 class TrackCell: UITableViewCell {
+    
+    var trackInfo: Track! {
+        didSet {
+            
+            if let price = trackInfo.trackPrice {
+                buyButton.setTitle("$\(price)", forState: .Normal)
+                
+            } else {
+                buyButton.hidden = true
+                
+            }
+            
+        }
+        
+    }
 
+    @IBOutlet weak var trackNameLabel: UILabel!
+    
+    @IBOutlet weak var playbutton: SVGPlayButton!
+    
+    @IBOutlet weak var buyButton: UIButton!
+    
+    @IBAction func pressedPlayButton(sender: AnyObject) {
+        print("pressed")
+        
+    }
+    
+    @IBAction func pressedBuyButton(sender: AnyObject) {
+        
+        if let urlString = trackInfo.trackViewURL {
+            if let url = NSURL(string: urlString) {
+                UIApplication.sharedApplication().openURL(url)
+                    
+            }
+            
+        }
+        
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
+        //this is a great time to setup the button
+        
+        playbutton.progressTrackColor = UIColor.lightGrayColor()
+        playbutton.progressColor = UIColor.darkGrayColor()
+        playbutton.playColor = UIColor.grayColor()
+        playbutton.pauseColor = UIColor.grayColor()
+        
+        playbutton.willPlay = { self.willPlayHandler() }
+        playbutton.willPause = { self.willPauseHandler() }
+        
+    }
+    
+    var player: AVAudioPlayer?
+    
+    private func willPlayHandler() {
+        print("WillPlay")
+        
+        if let data = trackInfo.mediaData ?? trackInfo.getMedia() {
+            player = player ?? (try? AVAudioPlayer(data: data))
+            
+        }
+        
+        player?.play()
+        
+    }
+    
+    private func willPauseHandler() {
+        print("willPause")
+        player?.pause()
+        
     }
 
     override func setSelected(selected: Bool, animated: Bool) {
